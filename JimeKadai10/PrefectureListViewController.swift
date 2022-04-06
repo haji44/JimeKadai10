f2c37df2e3b90602140cd22ab6a5a574ca3494f6
haji44 <61269325+haji44@users.noreply.github.com>
2022-04-06 19:13:49 +0900

    remove space

--- JimeKadai10/PrefectureListViewController.swift
+++ JimeKadai10/PrefectureListViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class PrefectureListViewController: UITableViewController {
-
     private let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
                        "山形県", "福島県", "茨城県", "栃木県", "群馬県",
                        "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
